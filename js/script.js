4d142b890cad21d7f9aeb70eddd8d80f67add30a
Nelus <niels.beekhuis@gmail.com>
2019-06-26 22:10:02 +0200

    added word field

--- js/script.js
+++ js/script.js
@@ -1,10 +1,14 @@
 $(document).ready(function(){
-    alfabet = ['A','B','C','D','E','F','G','H','I','J','K','L','M']
+    alfabet = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
 
     for (i = 0; i < alfabet.length; i++){
-        $( "#pagenation" ).append( '<li class="tab col s3"><a href="#'+alfabet[i]+'">'+alfabet[i]+'</a></li>' );
-        $( "#words" ).append( '<div id="'+alfabet[i]+'" class="col s12">'+alfabet[i]+'</div>' );
+        $( "#pagenation" ).append( '<li class="tab col"><a href="#'+alfabet[i]+'">'+alfabet[i]+'</a></li>' );
+        $( "#words" ).append( '<div id="'+alfabet[i]+'" class="col offset-s1 s10"><ul class="collapsible"></ul></div>' );
+        for (j = 0; j < alfabet.length; j++){
+            $( ".collapsible" ).append('<li><div class="collapsible-header default-primary-color">'+alfabet[j]+'</div><div class="collapsible-body"><span>Lorem ipsum dolor sit amet.</span></div></li>');
+        }
     }
-    
+
     $('.tabs').tabs();
+    $('.collapsible').collapsible();
   });
