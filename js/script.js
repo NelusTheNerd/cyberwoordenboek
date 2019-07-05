a95e7b34c7dc1e7004dc56fc528efa188e7d0b3a
Nelus <niels.beekhuis@gmail.com>
2019-07-05 12:25:53 +0200

    things

--- js/script.js
+++ js/script.js
@@ -2,15 +2,23 @@ $(document).ready(function(){
     alfabet = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
 
     for (i = 0; i < alfabet.length; i++){
-        $( "#pagenation" ).append( '<li class="tab col"><a href="#'+alfabet[i]+'">'+alfabet[i]+'</a></li>' );
-        $( "#words" ).append( '<div id="'+alfabet[i]+'" class="col offset-s1 s10"><ul class="collapsible"></ul></div>' );
+        letter = alfabet[i]
+        $( "#pagenation" ).append( '<li class="tab col"><a href="#'+letter+'">'+letter+'</a></li>' );
+        $( "#words" ).append( '<div id="'+letter+'" class="col offset-s1 s10"><ul class="collapsible"></ul></div>' );
 
-        $.getJSON("dictonary/"+alfabet[i]+".json", function(result){
-            $.each(result, function(i, field){
-              $("#"+alfbet[i]).append(field);
-            });
+        $.getJSON("http://localhost:8080/dictonary/"+alfabet[i]+".json", function(result){
+            console.log(result)
+            sessionStorage.setItem('A', result);
           });
+        
+          console.log(sessionStorage.getItem('A'))
+        //$.each(result[letter], function(index,value){
+          //  console.log("test")
+          //  console.log(value)
+            
+        //});
     }
+
     $('.tabs').tabs();
     $('.collapsible').collapsible();
   });
